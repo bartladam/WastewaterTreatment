5a489350831171b1e81d5e79b961f14929a5ed1a
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-28 23:30:25 +0200

    sewage water almost done

--- WastewaterTreatment/SewageWater.cs
+++ WastewaterTreatment/SewageWater.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,16 @@ namespace WastewaterTreatment
 {
     internal class SewageWater
     {
+        public int amountSand { get; set; }
+        public int amountGravel { get; set; }
+        public int largerParticles { get; set; }
+        public int organicPolutants { get; set; }
+        public SewageWater(int amountSand, int amountGravel, int largerParticles, int organicPolutants)
+        {
+            this.amountSand = amountSand;
+            this.amountGravel = amountGravel;
+            this.largerParticles = largerParticles;
+            this.organicPolutants = organicPolutants;
+        }
     }
 }
