d7ed4f5ed6ea9b36185a3849e882774e65dcb5b5
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-28 23:21:05 +0200

    classes complete

--- WastewaterTreatment/WastewaterTreatmentPlant.cs
+++ WastewaterTreatment/WastewaterTreatmentPlant.cs
@@ -8,5 +8,23 @@ namespace WastewaterTreatment
 {
     internal class WastewaterTreatmentPlant
     {
+        private PreliminaryTreatment preliminary { get; set; }
+        private PrimaryTreatments primary { get; set; }
+        private SecondaryTreatment secondary { get; set; }
+        private TertiaryTreatment? teritary { get; set; }
+        public WastewaterTreatmentPlant(PreliminaryTreatment preliminary, PrimaryTreatments primary, SecondaryTreatment secondary, TertiaryTreatment? tertiary)
+        {
+            this.preliminary = preliminary;
+            this.primary = primary;
+            this.secondary = secondary;
+            this.teritary = teritary;
+        }
+        public void Treatment(SewageWater water)
+        {
+            preliminary.Process(water);
+            primary.RemoveImpurity(water);
+            secondary.RemoveImpurity(water);
+
+        }
     }
 }
